d673805cfd4fd074c92c9accda3048f3842229b7
Kabarakh <git@kabarakh.de>
2013-06-29 16:46:31 +0200

    CHG: label of static typoscript

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Extended Tracking for Piwik');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', '[pt_piwik] Extended Tracking for Piwik');
 
 ?>
